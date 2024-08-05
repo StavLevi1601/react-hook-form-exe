8426f2253d7bcd4889d18d9d68d5c9306e8c1538
Stav Levi <37871649+StavLevi1601@users.noreply.github.com>
2024-08-05 19:07:46 +0300

    Update FormDialog.tsx

--- src/FormDialog.tsx
+++ src/FormDialog.tsx
@@ -1,58 +1,49 @@
-import React, { useState } from "react";
 import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-  DialogProps,
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	type DialogProps,
 } from "@mui/material";
-import { UseFormReturn } from "react-hook-form";
 
 interface FormDialogProps extends DialogProps {
-  onClose: () => void;
-  methods: UseFormReturn;
-  onSubmit: React.FormEventHandler<HTMLFormElement>;
-  error: boolean;
+	onClose: () => void;
+	onSubmit: () => void;
+	disabled: boolean;
 }
 
 export default function FormDialog({
-  children,
-  onClose,
-  methods,
-  onSubmit,
-  error,
-  ...rest
+	children,
+	onClose,
+	onSubmit,
+	disabled,
+	...rest
 }: FormDialogProps) {
-  const [diables, setDisables] = useState(false);
+	const handleCancel = () => {
+		onClose();
+	};
 
-  const handleCancel = () => {
-    methods.reset();
-    onClose();
-    setDisables(false);
-  };
-
-  const handleDisable = (isSubmitting: boolean, error: boolean) => {
-    return isSubmitting || error || diables;
-  };
-
-  return (
-    <Dialog {...rest} onClose={onClose}>
-      <DialogTitle>Form Dialog</DialogTitle>
-      <DialogContent>
-        <form onSubmit={onSubmit}>
-          {children}
-          <DialogActions>
-            <Button
-              type="submit"
-              disabled={handleDisable(methods.formState.isSubmitting, error)}
-            >
-              Submit
-            </Button>
-            <Button onClick={handleCancel}>Cancel</Button>
-          </DialogActions>
-        </form>
-      </DialogContent>
-    </Dialog>
-  );
+	return (
+		<Dialog {...rest} onClose={onClose}>
+			<DialogTitle>Form Dialog</DialogTitle>
+			<DialogContent>
+				{children}
+				<DialogActions>
+					<Button
+						type="submit"
+						variant="contained"
+						color="primary"
+						disabled={disabled}
+						onClick={onSubmit}
+					>
+						Submit
+					</Button>
+					<Button onClick={handleCancel} color="primary">
+						Cancel
+					</Button>
+				</DialogActions>
+			</DialogContent>
+		</Dialog>
+	);
 }
